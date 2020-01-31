becdd4d4b543a94995cd70a3a591f09f22b3be84
Matthew Loveday <mattloveday99@gmail.com>
2020-01-31 17:22:27 +0000

    UE4 Project Commit

---
+++ Source/GlobalGamesJam/GlobalGamesJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GlobalGamesJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOBALGAMESJAM_API AGlobalGamesJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
