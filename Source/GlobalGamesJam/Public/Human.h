6ed9b2ca9c5af9c25de138b2cee6ae9ed962cc94
JamieCooganCode <c013767h@student.staffs.ac.uk>
2020-02-02 03:43:19 +0000

    Human Half Finished

---
+++ Source/GlobalGamesJam/Public/Human.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickupActor.h"
+#include "Human.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOBALGAMESJAM_API AHuman : public APickupActor
+{
+	GENERATED_BODY()
+	
+};
